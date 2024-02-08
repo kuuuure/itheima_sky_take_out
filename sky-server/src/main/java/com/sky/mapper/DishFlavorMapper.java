9d112119cad9316e0d3d71948dc9c664654cca47
LHH <2530005847@qq.com>
2024-02-08 11:51:33 +0800

    删除，查询菜品业务开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -15,4 +16,18 @@ public interface DishFlavorMapper {
      */
     public void insertBatch(List<DishFlavor> flavors);
 
+
+    /**
+     * 根据 dish_id 删除菜品口味
+     * @param dishId
+     */
+    @Delete("delete from dish_flavor where dish_id=#{dishId}")
+    void deleteByDishId(Long dishId);
+
+
+    /**
+     * 根据 dish_id集合 删除菜品口味
+     * @param dishIds
+     */
+    void deleteByDishIds(List<Long> dishIds);
 }
