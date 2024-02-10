6d02af439f35e973037af9c993bdee1ece3c9737
LHH <2530005847@qq.com>
2024-02-10 11:31:47 +0800

    用户端登录，查看业务开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 
+import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -41,4 +42,6 @@ public interface SetmealDishMapper {
      */
     @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
     List<SetmealDish> getBySetmealId(Long setmealId);
+
+
 }
