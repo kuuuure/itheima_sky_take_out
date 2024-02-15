f47e0f621e748e4d8c72edee0ce5b515a7e941a7
LHH <2530005847@qq.com>
2024-02-15 21:27:03 +0800

    用户数据统计业务开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -21,4 +23,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+
+    /**
+     * 根据Map统计用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
